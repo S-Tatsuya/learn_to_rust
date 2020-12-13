c1ca3ea1bcd6071eb52bf7c833086ae56509bcf7
S.tatsuya <s.tatsuya0123@gmail.com>
2020-12-13 11:31:30 +0900

    3.5章 完了

--- src/rust_introduction/chapter3/scope/src/main.rs
+++ src/rust_introduction/chapter3/scope/src/main.rs
@@ -1,3 +1,18 @@
+struct Sample {
+	x: i32,
+}
+impl Sample {
+	fn new(x: i32) -> Sample {
+		Sample {x: x}
+	}
+	fn inc(&self) -> i32 {
+		self.x + 1
+	}
+	fn add(&self, x: i32) -> i32 {
+		self.x + x
+	}
+}
+
 fn main() {
     // scope block
     let x = 100;
@@ -13,16 +28,45 @@ fn main() {
     println!("{}", test(-1));
     println!("{}", test(50));
     println!("{}", test(101));
+
+	// strucp scope
+	let a = Sample::new(10);
+	let ans = a.inc();
+	println!("ans is {}", ans);
+	let ans = a.add(20);
+	println!("ans is {}", ans);
+
+	// closure scope
+	let num = 10;
+	let add_one = |x| {num + x};
+	let add_two = |x, y| {x + y};
+
+	let ans = add_one(1);
+	println!("ans is {}", ans);
+	let ans = add_two(10, 20);
+	println!("ans is {}", ans);
+	
+	// test
+	test2();
 }
 
 fn test(x: i32) -> i32 {
     // scope block
-    let mut ans = x;
-    if x < 0 {
-        ans = 0;
-    }
-    if x > 100 {
-        ans = 100;
-    }
+    let ans = if x < 0 {
+        0
+    } else if x > 100 {
+        100
+    } else {
+		x
+	};
     ans
+}
+
+fn test2() {
+	let num = 10;
+	fn add_one(x: i32, num: i32) -> i32 {
+		num + x
+	}
+	let ans = add_one(1, num);
+	println!("ans is {}", ans);
 }
